9e7cbb58959ab879cc279f42bcd4b959975c2bce
pablosky2086 <pablosky2086@gmail.com>
2026-01-13 17:53:31 +0100

    Actualizacion de la clase cliente

--- PeliuqueriApp/PeliuqueriApp/demo/src/main/java/com/example/demo/model/Cliente.java
+++ PeliuqueriApp/PeliuqueriApp/demo/src/main/java/com/example/demo/model/Cliente.java
@@ -1,11 +1,16 @@
 package com.example.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+
+import java.util.List;
 
 @Entity
 @Data
@@ -17,6 +22,11 @@ public class Cliente extends Usuario{
     private String telefono;
     private String observaciones;
     private String alergenos;
+
+    //Relacion con citas OneToMany y borrado en cascada
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Cita> citas;
 
     public Cliente(Usuario usuario) {
         this.setNombreCompleto(usuario.getNombreCompleto());
